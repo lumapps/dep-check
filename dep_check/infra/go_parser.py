090bef329586b536ad7bc05af2a7834fe562a46f
Ilan Piperno <ilan@lumapps.com>
2019-07-18 13:51:48 +0200

    Minor changes in go_parser.py

--- dep_check/infra/go_parser.py
+++ dep_check/infra/go_parser.py
@@ -1,4 +1,5 @@
 from ctypes import Structure, c_char_p, c_longlong, cdll
+import logging
 
 from dep_check.dependency_finder import IParser
 from dep_check.models import Dependencies, Dependency, SourceFile
@@ -7,21 +8,23 @@ from dep_check.models import Dependencies, Dependency, SourceFile
 class GoString(Structure):  # pylint: disable=too-few-public-methods
     _fields_ = [("data", c_char_p), ("n", c_longlong)]
 
-
-LIB = cdll.LoadLibrary("dep_check/lib/go_parse.so")
-LIB.FindDependencies.argtypes = [GoString]
-LIB.FindDependencies.restype = GoString
-
-
 class GoParser(IParser):
     """
     Implementation of the interface, to parse go
     """
+    def __init__(self):
+        try:
+            self.lib = cdll.LoadLibrary("dep_check/lib/go_parse.so")
+        except OSError:
+            logging.error("You have to build the go lib to parse go. Run 'make build-go'")
+            raise
+        self.lib.FindDependencies.argtypes = [GoString]
+        self.lib.FindDependencies.restype = GoString
 
     def find_dependencies(self, source_file: SourceFile) -> Dependencies:
         if source_file.code == "":
             return set()
-        deps_string = LIB.FindDependencies(
+        deps_string = self.lib.FindDependencies(
             GoString(source_file.code.encode(), len(source_file.code))
         )
         deps_list = deps_string.data.decode("utf-8").replace('"', "").split(";")[:-1]
