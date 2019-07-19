3b1fc09c397597a8560735bcb8c65000c6dc86fe
Ilan Piperno <ilan@lumapps.com>
2019-07-19 15:31:28 +0200

    Test clone go

--- setup.py
+++ setup.py
@@ -1,2 +1,7 @@
-import setuptools
-setuptools.setup()
+from setuptools import Extension, setup
+
+setup(
+    ext_modules=[
+        Extension('go_parse', ['dep_check/lib/go_parse.go'])
+    ],
+    build_golang={'root': 'github.com/lumapps/dep-check'})
