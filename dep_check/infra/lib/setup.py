47d993675a73b7efa2ddc3a13623b06432e9f745
Sébastien Boulle <sebastien.boulle@lumapps.com>
2020-08-27 23:30:47 +0200

    lint(infra): lint python file

--- dep_check/infra/lib/setup.py
+++ dep_check/infra/lib/setup.py
@@ -1,18 +1,17 @@
-from setuptools import setup, Extension
+from setuptools import Extension, setup
 
 setup(
-    name='goparse',
-    version='1.0',
-    description='Parses a file and returns its dependencies',
-    build_golang={'root': 'dep-check'},
+    name="goparse",
+    version="1.0",
+    description="Parses a file and returns its dependencies",
+    build_golang={"root": "dep-check"},
     ext_modules=[
         Extension(
-            'goparse',
-            sources=['find_dependencies.go'],
+            "goparse",
+            sources=["find_dependencies.go"],
             py_limited_api=True,
-            define_macros=[('Py_LIMITED_API', None)],
+            define_macros=[("Py_LIMITED_API", None)],
         )
     ],
-    setup_requires=['setuptools-golang'],
-
+    setup_requires=["setuptools-golang"],
 )
