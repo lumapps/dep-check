c7574151e0504072ddd92b7c6afb1b75edd45ebc
Nicolas Noirbent <nicolas.noirbent@lumapps.com>
2026-03-17 18:10:45 +0100

    chore(requirements): update black to v26.3.1

--- tests/fakefile.py
+++ tests/fakefile.py
@@ -4,35 +4,29 @@ from dep_check.models import Dependency, Module, SourceCode, SourceFile
 
 SIMPLE_FILE = SourceFile(
     module=Module("simple_module"),
-    code=SourceCode(
-        """
+    code=SourceCode("""
 import module
 import module.inside.module
 from amodule import aclass
-"""
-    ),
+"""),
 )
 FILE_WITH_LOCAL_IMPORT = SourceFile(
     module=Module("amodule.local_module"),
-    code=SourceCode(
-        """
+    code=SourceCode("""
 import module
 import module.inside.module
 from . import aclass
 from .inside import aclass
-"""
-    ),
+"""),
 )
 FILE_WITH_STD_IMPORT = SourceFile(
     module=Module("amodule.std_module"),
-    code=SourceCode(
-        """
+    code=SourceCode("""
 import module
 import module.inside.module
 import itertools
 from abc import ABC
-"""
-    ),
+"""),
 )
 
 GLOBAL_DEPENDENCIES = {
